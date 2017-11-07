bc6dad7d0ca94c33ce7c3883edb0371e2bf25ac0
Mauro Titimoli <mgtitimoli@gmail.com>
2017-11-07 17:13:46 -0300

    chore: Remove unnecessary scripts and add npm hooks

--- package-scripts.js
+++ package-scripts.js
@@ -1,13 +1,14 @@
 // prettier-ignore
 module.exports = {
   scripts: {
-    "flow-copy-source": "flow-copy-source -v src",
     "build:clean": "rm -rfv dist compat",
     "build:flow": "nps 'flow-copy-source dist' 'flow-copy-source compat/cjs'",
     "build:index": "create-index --banner '// @flow' src/ && sed -f indexIgnore.sed -i src/index.js && nps 'lint --fix src/index.js'",
     "build:readme": "pkg-to-readme --template ./readmeTemplate.ejs --force && documentation readme src/** --section API && doctoc README.md",
     "build:src": "rollup -c ../../rollup.config.js",
-    "dist": "nps 'build:clean' 'build:index' 'build:src' 'build:flow' 'build:readme'",
-    "lint": "eslint --rule 'flowtype-errors/show-errors: error'"
+    "flow-copy-source": "flow-copy-source -v src",
+    "lint": "eslint --rule 'flowtype-errors/show-errors: error'",
+    "prepublishOnly": "nps 'build:clean' 'build:src' 'build:flow'",
+    "version": "nps 'build:index' 'build:readme' && git add README.md src/index.js",
   }
 };
