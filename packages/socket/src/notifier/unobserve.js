36e42243f56c4d587c89f477964b23f032b680fd
Mauro Titimoli <mgtitimoli@gmail.com>
2018-12-12 15:08:20 -0300

    fix: Fix typo

--- packages/socket/src/notifier/unobserve.js
+++ packages/socket/src/notifier/unobserve.js
@@ -5,7 +5,7 @@ import {remove as arrayRemove} from "@jumpn/utils-array";
 import type {Notifier, Observer} from "./types";
 
 const removeObserver = (observers, observer) =>
-  arrayRemove(observers.indexOf(observer), 1, observer);
+  arrayRemove(observers.indexOf(observer), 1, observers);
 
 const unobserve = <Result, Variables: void | Object>(
   {activeObservers, ...rest}: Notifier<Result, Variables>,
