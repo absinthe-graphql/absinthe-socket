0001b523be3edcff1e292ddfae65704d9323af11
Mauro Titimoli <mgtitimoli@gmail.com>
2018-12-12 15:05:57 -0300

    chore: Remove rollup-plugin-terser as it breaks with babel

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import commonjs from "rollup-plugin-commonjs";
 import globby from "globby";
 import pascalCase from "pascal-case";
 import resolve from "rollup-plugin-node-resolve";
-import {terser} from "rollup-plugin-terser";
 
 // $FlowFixMe
 const pkg = require(`${process.cwd()}/package.json`);
@@ -25,75 +24,67 @@ const plugins = {
   }),
   commonjs: commonjs({
     namedExports: {
-      // "apollo-utilities": [
-      //   "addTypenameToDocument",
-      //   "argumentsObjectFromField",
-      //   "assign",
-      //   "checkDocument",
-      //   "cloneDeep",
-      //   "createFragmentMap",
-      //   "flattenSelections",
-      //   "getDefaultValues",
-      //   "getDirectiveInfoFromField",
-      //   "getDirectiveNames",
-      //   "getEnv",
-      //   "getFragmentDefinition",
-      //   "getFragmentDefinitions",
-      //   "getFragmentQueryDocument",
-      //   "getMainDefinition",
-      //   "getMutationDefinition",
-      //   "getOperationDefinition",
-      //   "getOperationDefinitionOrDie",
-      //   "getOperationName",
-      //   "getQueryDefinition",
-      //   "getStoreKeyName",
-      //   "graphQLResultHasError",
-      //   "hasDirectives",
-      //   "isDevelopment",
-      //   "isEnv",
-      //   "isEqual",
-      //   "isField",
-      //   "isIdValue",
-      //   "isInlineFragment",
-      //   "isJsonValue",
-      //   "isNumberValue",
-      //   "isProduction",
-      //   "isScalarValue",
-      //   "isTest",
-      //   "maybeDeepFreeze",
-      //   "removeConnectionDirectiveFromDocument",
-      //   "removeDirectivesFromDocument",
-      //   "resultKeyNameFromField",
-      //   "shouldInclude",
-      //   "storeKeyNameFromField",
-      //   "toIdValue",
-      //   "tryFunctionOrLogError",
-      //   "valueFromNode",
-      //   "valueToObjectRepresentation",
-      //   "variablesInOperation",
-      //   "warnOnceInDevelopment"
-      // ],
+      "apollo-utilities": [
+        "addTypenameToDocument",
+        "argumentsObjectFromField",
+        "assign",
+        "checkDocument",
+        "cloneDeep",
+        "createFragmentMap",
+        "flattenSelections",
+        "getDefaultValues",
+        "getDirectiveInfoFromField",
+        "getDirectiveNames",
+        "getEnv",
+        "getFragmentDefinition",
+        "getFragmentDefinitions",
+        "getFragmentQueryDocument",
+        "getMainDefinition",
+        "getMutationDefinition",
+        "getOperationDefinition",
+        "getOperationDefinitionOrDie",
+        "getOperationName",
+        "getQueryDefinition",
+        "getStoreKeyName",
+        "graphQLResultHasError",
+        "hasDirectives",
+        "isDevelopment",
+        "isEnv",
+        "isEqual",
+        "isField",
+        "isIdValue",
+        "isInlineFragment",
+        "isJsonValue",
+        "isNumberValue",
+        "isProduction",
+        "isScalarValue",
+        "isTest",
+        "maybeDeepFreeze",
+        "removeConnectionDirectiveFromDocument",
+        "removeDirectivesFromDocument",
+        "resultKeyNameFromField",
+        "shouldInclude",
+        "storeKeyNameFromField",
+        "toIdValue",
+        "tryFunctionOrLogError",
+        "valueFromNode",
+        "valueToObjectRepresentation",
+        "variablesInOperation",
+        "warnOnceInDevelopment"
+      ],
       phoenix: ["Ajax", "Channel", "LongPoll", "Presence", "Socket"]
     }
   }),
-  resolve: resolve(),
-  terser: terser()
+  resolve: resolve()
 };
 
-const getEsConfig = fileName => ({
+const getCjsAndEsConfig = fileName => ({
   input: `${dirs.input}/${fileName}`,
-  output: {file: `${dirs.output}/${fileName}`, format: "es", sourcemap: true},
-  plugins: [plugins.babel, plugins.terser]
-});
-
-const getCjsConfig = fileName => ({
-  input: `${dirs.input}/${fileName}`,
-  output: {
-    file: `${dirs.compat}/cjs/${fileName}`,
-    format: "cjs",
-    sourcemap: true
-  },
-  plugins: [plugins.babel, plugins.terser]
+  output: [
+    {file: `${dirs.output}/${fileName}`, format: "es", sourcemap: true},
+    {file: `${dirs.compat}/cjs/${fileName}`, format: "cjs", sourcemap: true}
+  ],
+  plugins: [plugins.babel]
 });
 
 const sources = globby.sync("**/*js", {cwd: dirs.input});
@@ -114,11 +105,7 @@ export default [
       name: pascalCase(getUnscopedName(pkg)),
       sourcemap: true
     },
-    plugins: [plugins.babel, plugins.resolve, plugins.commonjs, plugins.terser]
+    plugins: [plugins.babel, plugins.resolve, plugins.commonjs]
   },
-  // TODO
-  // Use multiple outputs once this gets merged:
-  // https://github.com/TrySound/rollup-plugin-terser/pull/16
-  ...sources.map(getCjsConfig),
-  ...sources.map(getEsConfig)
+  ...sources.map(getCjsAndEsConfig)
 ];
